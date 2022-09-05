33377e6ea2906e3b0c96053646c989c727fdfc5f
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-09-05 21:52:56 +0800

    ci: use toolchain file on Linux

---
+++ cmmkv/cmake/linux-aarch64-toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
+set(CMAKE_ASM_COMPILER "aarch64-linux-gnu-as")
+set(CMAKE_ASM-ATT_COMPILER "aarch64-linux-gnu-as")
